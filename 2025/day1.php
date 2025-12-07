9d0a3c57ad1ec0f143acdebec05cdc20bda4a733
faizan619 <alamf6023@gmail.com>
2025-12-07 08:05:51 +0530

    done with day 1 part 1

--- 2025/day1.php
+++ 2025/day1.php
@@ -6,14 +6,34 @@ ini_set('display_errors', 1);
 require './FetchData.php';
 
 $curr_val = 50;
+$zero_count = 0;
+
+
+// $test = 104 % 100;
+// $test = -324 %100;
+// echo $test+=100;
 
 foreach ($steps as $key => $val) {
-	if (substr($val, 0, 1) === 'R') {  
-        
+    $a = $val[0];
+    $num = substr($val,1);
+	if ($a === 'R') {  
+        // echo "First : ".$num.'</br>';
+        echo "R - ". $curr_val + $num.'</br>';
+        // $curr_val = ($curr_val + $num) % 100;
     }
-    elseif (substr($val, 0, 1) === 'L') {  
-        
+    elseif ($a === 'L') {  
+        // echo "Second : ".$num.'</br>';
+        // echo "L - ".$curr_val - $num.'</br>';
+
+        // $curr_val = ($curr_val - $num) % 100;
+        // if ($curr_val < 0) {
+        //     $curr_val += 100;
+        // }
+    }
+    if ($curr_val === 0) {
+        $zero_count++;
     }
 }
+echo "Total : ".$zero_count;
 
 ?>
